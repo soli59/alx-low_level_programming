d87aa58573b9ca172540ff45941a815967f9a3ba
soli59 <mohammedsaad0509@gmail.com>
2023-09-07 16:49:50 -0700

    hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,11 @@
 
 /**
  * main - Entry point
- * 
- * Description:A C Programming is like building a multilingual puzzle
- *
- * Return always 0 (success)
- */        
- int main(void); 
-{	
+ * Description: A C program is like building a multilingual puzzle.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
