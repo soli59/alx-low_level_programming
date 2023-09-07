9f730ff9181ba6f77f1d70104ad643639b87e8a5
soli59 <mohammedsaad0509@gmail.com>
2023-09-07 16:16:40 -0700

    hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-     int main(void) {	
+/**
+ * main-Entry point
+ * 
+ * Description
+ *
+ * Return always:0 (done)
+ */        
+        int main(void) {	
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 	}
