07acd1b5feb0f0e9949432e7df5852965d2c709d
soli59 <mohammedsaad0509@gmail.com>
2023-09-08 12:53:04 -0700

    hello_again

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints single-digit numbers in base 10 from 0 to 9.
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+
+	putchar('\n');
+	return (0);
+}
