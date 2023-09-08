06fb3bd403259363e86bf43695e21e42c9ce762e
soli59 <mohammedsaad0509@gmail.com>
2023-09-07 17:22:37 -0700

    hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- * Description: A C program that prints with printf function.
- * Return Always 0 (Success)
-*/
-
+ *
+ * Description: A C program that prints with the printf function.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
