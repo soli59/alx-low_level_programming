29e14fe1e867adeba72f349691ee0828a9cec8d9
soli59 <mohammedsaad0509@gmail.com>
2023-09-08 13:03:55 -0700

    hello_again

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,17 +9,14 @@
 */
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
-	putchar('\n');
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+
+		putchar('\n');
 
 	return (0);
 }
