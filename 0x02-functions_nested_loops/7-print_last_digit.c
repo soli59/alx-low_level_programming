2e8a839a880cc978fa26b6be24457974834ccecc
soli59 <mohammedsaad0509@gmail.com>
2023-09-12 09:53:45 -0700

    hello_world

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,13 @@
 int print_last_digit(int n)
 
 {
-	int last_digit = n % 10
+	int last_digit;
 
-	if (last_digit < 0)
-	last_digit = -last_digit;
+	if (n < 0)
+		last_digit = -1 * (n % 10);
+	else
+		last_digit = n % 10;
 
-	printf("%d\n", last_digit);
-
-	return  (last_digit);
+	_putcher(last_digit + '0');
+	return (last_digit);
 }
