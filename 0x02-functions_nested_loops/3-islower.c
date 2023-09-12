3e3f71dd330c04ce080381d20bc32e24e446aa5b
soli59 <mohammedsaad0509@gmail.com>
2023-09-12 07:49:26 -0700

    hello_world

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,12 +5,12 @@
  * @c: The character to check
  *
  * Return: 1 if c is lowercase, 0 otherwise
- */
+*/
 int _islower(int c)
 
 {
-    if (c >= 97 && c <= 122)
-        return (1);
-    else
-	return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
