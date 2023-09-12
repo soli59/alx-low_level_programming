e943bc9f83aa777fd08a4be97889b92d1206f318
soli59 <mohammedsaad0509@gmail.com>
2023-09-12 10:46:52 -0700

    hello_world

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+
+{
+
+int i, j, result;
+
+		for (i = 0; i <= 9; i++)
+		{
+		for (j = 0; j <= 9; j++)
+		{
+		result = i * j;
+		if (j == 0)
+		{
+		_putchar('0');
+		}
+		else if (result < 10)
+		{
+		_putchar(' ');
+		_putchar(result + '0');
+		}
+		else
+		{
+		_putchar((result / 10) + '0');
+		_putchar((result % 10) + '0');
+		}
+		if (j < 9)
+		{
+		_putchar(',');
+		_putchar(' ');
+		}
+	}
+	_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,6 +19,7 @@ int _abs(int);
 
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
 
 
 #endif
