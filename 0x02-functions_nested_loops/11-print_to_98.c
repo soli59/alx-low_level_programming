ec2dd74f530eb602c0021929ef430a578defe7b1
soli59 <mohammedsaad0509@gmail.com>
2023-09-12 11:09:44 -0700

    hello_world

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,14 +1,31 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * add - Adds two integers and returns the result.
- * @a: The first integer
- * @b: The second integer
- *
- * Return: The sum of a and b
+ * print_to_98 - Prints natural numbers from n to 98.
+ * @n: The starting number
  */
-int add(int a, int b)
-
+void print_to_98(int n)
 {
-	return (a + b);
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	printf("\n");
 }
