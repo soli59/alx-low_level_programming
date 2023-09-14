0219f61b0d7c169c05640d37f92a5d2e82539795
soli59 <mohammedsaad0509@gmail.com>
2023-09-14 13:05:22 -0700

    hello_world

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,14 +4,16 @@
  * main - Entry point for the Fizz-Buzz program.
  *
  * Description:
- *This program prints numbers from 1 to 100, replacing multi of 3 with "Fizz,"
- *multi of 5 with "Buzz," and multi of both with "FizzBuzz."
+ *	This program prints numbers from 1 to 100, replacing multi of 3 with "Fizz,"
+ *	multi of 5 with "Buzz," and multi of both with "FizzBuzz."
  *
  * Return: Always 0 (Success)
 */
 int main(void)
 {
-	for (int i = 1; i <= 100; i++)
+	int i;
+
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
