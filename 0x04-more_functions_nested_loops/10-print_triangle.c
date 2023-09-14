cb1a77f7e4072ef58997d623a5bc5fefea07d0b6
soli59 <mohammedsaad0509@gmail.com>
2023-09-14 13:14:58 -0700

    hello_world

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle of '#' characters in the terminal.
+ * @size: The size of the triangle.
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int row, col;
+
+		for (row = 1; row <= size; row++)
+		{
+			for (col = 1; col <= row; col++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
