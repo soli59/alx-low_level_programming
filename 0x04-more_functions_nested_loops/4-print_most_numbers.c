1982ce519805f03d25e9ff80fdd8675b3621985a
soli59 <mohammedsaad0509@gmail.com>
2023-09-14 05:31:50 -0700

    hello_world

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints numbers from 0 to 9
- * @c@c(excluding 2 and 4) followed by a new line.
+ * print_numbers - Prints numbers from 0 to 9 (excluding 2 and 4)
+ * followed by a new line.
 */
 void print_numbers(void)
 {
@@ -10,10 +10,10 @@ void print_numbers(void)
 
 	for (digit = '0'; digit <= '9'; digit++)
 	{
-	if (digit != '2' && digit != '4)
-		{
-		_putchar(digit);
-		}
+	if (digit != '2' && digit != '4')
+	{
+	_putchar(digit);
+	}
 	}
 
 	_putchar('\n');
