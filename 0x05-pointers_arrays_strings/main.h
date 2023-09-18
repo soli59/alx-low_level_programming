fcd3a29b74c5e013b7c59f7945cc5875e8804837
soli59 <mohammedsaad0509@gmail.com>
2023-09-18 10:19:40 -0700

    hello_world

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,16 +4,16 @@
 #include <stdio.h>
 
 int _putchar (char c);
-void reset_to_98 (int *n);
-void swap_int (int *a, int *b);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
-void puts (char *str);
-void print_rev (char *s);
-void rev_string (char *s);
-void puts2 (char *str);
-void puts half (char *str);
-void print_array (int *a, int n);
-char *_strcpy (char *dest, char *src);
+void puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 int atoi(char *s);
 
-#endif /* MAIN_H */
+#endif
