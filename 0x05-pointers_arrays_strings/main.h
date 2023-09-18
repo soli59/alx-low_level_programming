887caea2e7437a58e1be76e1a8a9f254f41bdfb5
soli59 <mohammedsaad0509@gmail.com>
2023-09-18 10:25:57 -0700

    hello_world

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,9 @@
-#ifndef "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
-int _putchar (char c);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,4 +16,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int atoi(char *s);
 
-#endif
+#endif /* MAIN_H */
+
