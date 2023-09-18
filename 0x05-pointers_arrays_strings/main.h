d575743374f0cb88fb0b9c4f44693aa25247bd86
soli59 <mohammedsaad0509@gmail.com>
2023-09-18 10:29:02 -0700

    hello_world

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void puts(char *str);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
@@ -17,4 +17,3 @@ char *_strcpy(char *dest, char *src);
 int atoi(char *s);
 
 #endif /* MAIN_H */
-
