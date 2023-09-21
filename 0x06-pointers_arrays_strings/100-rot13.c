82df122f4d0d146cb21839f11f3dba56bfdff93b
soli59 <mohammedsaad0509@gmail.com>
2023-09-21 13:13:33 -0700

    hello_world

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13 cipher.
+ * @str: The string to encode.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; input[j] != '\0'; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
