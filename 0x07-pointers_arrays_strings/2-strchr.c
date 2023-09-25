d09f6d2b34f60badea9fbf00a56cf082e0570ecc
soli59 <mohammedsaad0509@gmail.com>
2023-09-25 11:08:34 -0700

    hello_world

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,16 +1,16 @@
 /**
-* _strchr - Locates a character in a string.
-* @s: Pointer to the string.
-* @c: Character to locate.
-*
-* Return: Pointer to the first occurrence of @c in @s, or NULL if not found.
-*/
+ * _strchr - Locates a character in a string.
+ * @s: Pointer to the string.
+ * @c: Character to locate.
+ *
+ * Return: Pointer to the first occurrence of @c in @s, or NULL if not found.
+ */
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
 		if (*s == c)
-			return s;
+			return (s);
 		s++;
 	}
 
