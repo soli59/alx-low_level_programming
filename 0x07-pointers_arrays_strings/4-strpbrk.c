fd7df3da063d925a0d8cba520b2d3a048e61d18a
soli59 <mohammedsaad0509@gmail.com>
2023-09-25 11:58:18 -0700

    hello_world

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,15 +14,18 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
+	char *p;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				return (s + i);
+			{
+				p = &s[i];
+				return (p);
 		}
 	}
 
-	return (NULL);
+	return (0);
 }
