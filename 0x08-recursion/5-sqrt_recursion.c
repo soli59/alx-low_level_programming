fa97e3faa96203a4f575de61a7de151d3e212a28
soli59 <mohammedsaad0509@gmail.com>
2023-09-28 12:01:48 -0700

    hello_world

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,14 @@
 /**
+ * _sqrt_helper - Helper function to calculate the square root recursively.
+ * @n: The number to calculate the square root of.
+ * @guess: The current guess for the square root.
+ *
+ * Return: The natural square root of n.
+ * If n does not have a natural square root, returns -1.
+ */
+int _sqrt_helper(int n, int guess);
+
+/**
  * _sqrt_recursion - Returns the natural square root of a number.
  * @n: The number to calculate the square root of.
  *
@@ -13,18 +23,18 @@ int _sqrt_recursion(int n)
 	if (n == 0 || n == 1)
 		return (n);
 
-	return (sqrt_helper(n, 1));
+	return (_sqrt_helper(n, 1)); // Helper function for calculation.
 }
 
 /**
- * sqrt_helper - Helper function to calculate the square root recursively.
+ * _sqrt_helper - Helper function to calculate the square root recursively.
  * @n: The number to calculate the square root of.
  * @guess: The current guess for the square root.
  *
  * Return: The natural square root of n.
  * If n does not have a natural square root, returns -1.
  */
-int sqrt_helper(int n, int guess)
+int _sqrt_helper(int n, int guess)
 {
 	if (guess * guess == n)
 		return (guess);
@@ -32,5 +42,5 @@ int sqrt_helper(int n, int guess)
 	if (guess * guess > n)
 		return (-1);
 
-	return (sqrt_helper(n, guess + 1));
+	return (_sqrt_helper(n, guess + 1));
 }
