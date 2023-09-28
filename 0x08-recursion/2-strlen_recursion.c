43645b82ddd3dc71b719801606c4e51859f02738
soli59 <mohammedsaad0509@gmail.com>
2023-09-28 11:46:30 -0700

    hello_world

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,8 +8,11 @@
 */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0'
-		return (0);
+int ntr = 0;
+if (*s > '\0')
+{
+	ntr += _strlen_recursion(s + 1) + 1;
+}
 
-	return (1 + _strlen_recursion(s + 1));
+return (ntr);
 }
