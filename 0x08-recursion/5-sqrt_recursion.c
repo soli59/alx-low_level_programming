e00e092c94e12f536272ade6efd43fb25e54c896
soli59 <mohammedsaad0509@gmail.com>
2023-09-28 11:58:44 -0700

    hello_world

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to calculate the square root of.
+ *
+ * Return: The natural square root of n.
+ * If n does not have a natural square root, returns -1.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0 || n == 1)
+		return (n);
+
+	return (sqrt_helper(n, 1));
+}
+
+/**
+ * sqrt_helper - Helper function to calculate the square root recursively.
+ * @n: The number to calculate the square root of.
+ * @guess: The current guess for the square root.
+ *
+ * Return: The natural square root of n.
+ * If n does not have a natural square root, returns -1.
+ */
+int sqrt_helper(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+
+	if (guess * guess > n)
+		return (-1);
+
+	return (sqrt_helper(n, guess + 1));
+}
