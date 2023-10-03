c2d8da02b6fd7f84edd247fa43c76771e777b4d4
soli59 <mohammedsaad0509@gmail.com>
2023-10-03 06:53:25 -0700

    hello_world

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of pointers to the command-line arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	int coins[] = {25, 10, 5, 2, 1};
+	int numCoins = 0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		while (cents >= coins[i])
+		{
+			cents -= coins[i];
+			numCoins++;
+		}
+	}
+
+	printf("%d\n", numCoins);
+	return (0);
+}
