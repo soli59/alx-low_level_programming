d77323015fd9524c0ac78f20db668cb9c4f4fe9d
soli59 <mohammedsaad0509@gmail.com>
2023-10-10 09:07:52 -0700

    typedif_structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,10 +4,13 @@
 /**
  * print_dog - Print the elements of a struct dog
  * @d: Pointer to the struct dog to be printer
+ *
+ * Retuen: void.
  */
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d == NULL)
+		return;
 	{
 	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
