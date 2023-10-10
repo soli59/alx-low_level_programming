d7ff43bac9ecb43162a289ab463532bc05264489
soli59 <mohammedsaad0509@gmail.com>
2023-10-10 08:35:59 -0700

    typedif_structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_STRUCT_H
+#define DOG_STRUCT_H
+
+/**
+ * struct dog - Structure representing a dog
+ * @name: Name of the dog (string)
+ * @age: Age of the dog (float)
+ * @owner: Owner of the dog (string)
+ *
+ * Description: This structure defines a dog with its name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
