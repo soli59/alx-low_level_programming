4055726de6544acec0e53079d9a1258b03d291df
soli59 <mohammedsaad0509@gmail.com>
2023-10-10 08:41:53 -0700

    typedif_structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h" /* Include the header file with the struct definition */
+
+/**
+ * init_dog - Initialize a struct dog
+ * @d: Pointer to the struct dog to be initialized
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This function initializes a struct dog with the provided values.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
