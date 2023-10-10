907102e3b33c5185725a44a07168991164595f1d
soli59 <mohammedsaad0509@gmail.com>
2023-10-10 08:57:48 -0700

    typedif_structure

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h" /* Include the header file with the struct definition */
+
+/**
+ * print_dog - Print the elements of a struct dog
+ * @d: Pointer to the struct dog to be printed
+ *
+ * Description: This function prints the elements of a struct dog in a specific format.
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	}
+}
